89865cb646d701d06657bd932bf20442682e8e47
atalay <atalaybeyazit16@gmail.com>
2024-11-04 18:37:39 +0300

    AutoMapper ve DbContext tamamlandı.

---
+++ KTUN_DbContext.cs
@@ -0,0 +1,208 @@
+﻿namespace KTUN_Final_Year_Project
+{
+    using KTUN_Final_Year_Project.Entities;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Migrations;
+    using System.Reflection.Emit;
+    public class KTUN_DbContext : DbContext
+    {
+#nullable disable
+        public DbSet<SalesDetails> SalesDetails { get; set; }
+
+        public DbSet<Sales> Sales { get; set; }
+
+        public DbSet<UserLoyalty> UserLoyalty { get; set; }
+
+        public DbSet<ProductRecommendations> ProductRecommendations { get; set; }
+
+        public DbSet<UserStore> UserStore { get; set; }
+
+        public DbSet<CustomerFeedback> CustomerFeedback { get; set; }
+
+        public DbSet<Users> Users { get; set; }
+
+        public DbSet<Inventory> Inventory { get; set; }
+
+        public DbSet<ProductSuppliers> ProductSuppliers { get; set; }
+
+        public DbSet<Products> Products { get; set; }
+
+        public DbSet<Stores> Stores { get; set; }
+
+        public DbSet<Categories> Categories { get; set; }
+
+        public DbSet<Suppliers> Suppliers { get; set; }
+
+        public DbSet<LoyaltyPrograms> LoyaltyPrograms { get; set; }
+
+        public KTUN_DbContext(DbContextOptions<KTUN_DbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder Modelbuilder)
+        {
+            Modelbuilder.Entity<SalesDetails>().HasKey(sd => sd.SaleDetailID);
+            Modelbuilder.Entity<SalesDetails>().ToTable("SalesDetails");
+
+            Modelbuilder.Entity<Sales>().HasKey(s => s.SaleID);
+            Modelbuilder.Entity<Sales>().ToTable("Sales");
+
+            Modelbuilder.Entity<UserLoyalty>().HasKey(ul => ul.UserLoyaltyID);
+            Modelbuilder.Entity<UserLoyalty>().ToTable("UserLoyalty");
+
+            Modelbuilder.Entity<ProductRecommendations>().HasKey(pr => pr.RecommendationID);
+            Modelbuilder.Entity<ProductRecommendations>().ToTable("ProductRecommentdations");
+
+            Modelbuilder.Entity<UserStore>().HasKey(us => us.UserStoreID);
+            Modelbuilder.Entity<UserStore>().ToTable("UserStore");
+
+            Modelbuilder.Entity<CustomerFeedback>().HasKey(cf => cf.CustomerFeedbackID);
+            Modelbuilder.Entity<CustomerFeedback>().ToTable("CustomerFeedback");
+
+            Modelbuilder.Entity<Users>().HasKey(u => u.UserID);
+            Modelbuilder.Entity<Users>().HasIndex(u => u.NFC_CardID).IsUnique();
+            Modelbuilder.Entity<Users>().ToTable("Users");
+
+            Modelbuilder.Entity<Inventory>().HasKey(i => i.InventoryID);
+            Modelbuilder.Entity<Inventory>().ToTable("Inventory");
+
+            Modelbuilder.Entity<ProductSuppliers>().HasKey(ps => ps.ProductSupplierID);
+            Modelbuilder.Entity<ProductSuppliers>().ToTable("ProductSuppliers");
+
+            Modelbuilder.Entity<Products>().HasKey(p => p.ProductID);
+            Modelbuilder.Entity<Products>().ToTable("Products");
+
+            Modelbuilder.Entity<Stores>().HasKey(st => st.StoreID);
+            Modelbuilder.Entity<Stores>().ToTable("Stores");
+
+            Modelbuilder.Entity<Categories>().HasKey(c => c.CategoryID);
+            Modelbuilder.Entity<Categories>().ToTable("Categories");
+
+            Modelbuilder.Entity<Suppliers>().HasKey(su => su.SupplierID);
+            Modelbuilder.Entity<Suppliers>().ToTable("Suppliers");
+
+            Modelbuilder.Entity<LoyaltyPrograms>().HasKey(lp => lp.LoyaltyProgramsID);
+            Modelbuilder.Entity<LoyaltyPrograms>().ToTable("LoyaltyPrograms");
+
+
+            Modelbuilder.Entity<SalesDetails>()
+            .HasOne(sd => sd.Sale)
+            .WithMany()
+            .HasForeignKey(sd => sd.SaleID);
+
+            Modelbuilder.Entity<SalesDetails>()
+            .HasOne(sd => sd.Store)
+            .WithMany()
+            .HasForeignKey(sd => sd.StoreID);
+
+
+
+            Modelbuilder.Entity<Sales>()
+            .HasOne(s => s.User)
+            .WithMany()
+            .HasForeignKey(s => s.UserID)
+            .HasPrincipalKey(u => u.UserID);
+
+            Modelbuilder.Entity<Sales>()
+            .HasOne(s => s.Store)
+            .WithMany()
+            .HasForeignKey(s => s.StoreID);
+
+
+
+
+            Modelbuilder.Entity<UserLoyalty>()
+            .HasOne(ul => ul.User)
+            .WithMany()
+            .HasForeignKey(ul => ul.UserID)
+            .HasPrincipalKey(u => u.UserID);
+
+
+            Modelbuilder.Entity<UserLoyalty>()
+             .HasOne(ul => ul.LoyaltyProgram)
+             .WithMany()
+             .HasForeignKey(ul => ul.LoyaltyProgramID);
+
+
+
+
+            Modelbuilder.Entity<ProductRecommendations>()
+            .HasOne(pr => pr.User)
+            .WithMany()
+            .HasForeignKey(pr => pr.UserID)
+            .HasPrincipalKey(u => u.UserID);
+
+            Modelbuilder.Entity<ProductRecommendations>()
+           .HasOne(pr => pr.Product)
+           .WithMany()
+           .HasForeignKey(pr => pr.ProductID);
+
+
+
+
+            Modelbuilder.Entity<UserStore>()
+            .HasOne(us => us.User)
+            .WithMany()
+            .HasForeignKey(us => us.UserID)
+            .HasPrincipalKey(u => u.UserID);
+
+            Modelbuilder.Entity<UserStore>()
+           .HasOne(us => us.Store)
+           .WithMany()
+           .HasForeignKey(us => us.StoreID);
+
+
+
+
+            Modelbuilder.Entity<CustomerFeedback>()
+            .HasOne(cf => cf.User)
+            .WithMany()
+            .HasForeignKey(cf => cf.UserID)
+            .HasPrincipalKey(u => u.UserID);
+
+            Modelbuilder.Entity<CustomerFeedback>()
+            .HasOne(cf => cf.Product)
+            .WithMany()
+            .HasForeignKey(cf => cf.ProductID);
+
+
+
+
+            Modelbuilder.Entity<Inventory>()
+            .HasOne(i => i.Product)
+            .WithMany()
+            .HasForeignKey(i => i.ProductID);
+
+
+
+
+            Modelbuilder.Entity<ProductSuppliers>()
+            .HasOne(ps => ps.Supplier)
+            .WithMany()
+            .HasForeignKey(ps => ps.SupplierID);
+
+            Modelbuilder.Entity<ProductSuppliers>()
+            .HasOne(ps => ps.Product)
+            .WithMany()
+            .HasForeignKey(ps => ps.ProductID);
+
+
+
+            Modelbuilder.Entity<Products>()
+            .HasOne(p => p.Store)
+            .WithMany()
+            .HasForeignKey( p => p.StoreID);
+
+
+            Modelbuilder.Entity<Products>()
+            .HasOne(p => p.Category)
+            .WithMany()
+            .HasForeignKey(p => p.CategoryID);
+
+
+
+            base.OnModelCreating(Modelbuilder);
+        }
+    }
+}
---
+++ Mapper/AutoMapper.cs
@@ -0,0 +1,102 @@
+﻿using AutoMapper;
+using KTUN_Final_Year_Project.DTOs;
+using KTUN_Final_Year_Project.Entities;
+using KTUN_Final_Year_Project.ResponseDTOs;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace KTUN_Final_Year_Project.Mapper // Replace with your namespace  
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+
+            CreateMap<SalesDetailsDTO, SalesDetails>();
+            CreateMap<SalesDetails, SalesDetailsDTO>();
+
+            CreateMap<SalesDTO, Sales>();
+            CreateMap<Sales, SalesDTO>();
+
+            CreateMap<UserLoyaltyDTO, UserLoyalty>();
+            CreateMap<UserLoyalty, UserLoyaltyDTO>();
+
+            CreateMap<ProductRecommendationsDTO, ProductRecommendations>();
+            CreateMap<ProductRecommendations, ProductRecommendationsDTO>();
+
+            CreateMap<UserStoreDTO, UserStore>();
+            CreateMap<UserStore, UserStoreDTO>();
+
+            CreateMap<CustomerFeedbackDTO, CustomerFeedback>();
+            CreateMap<CustomerFeedback, CustomerFeedbackDTO>();
+
+            CreateMap<UsersDTO, Users>();
+            CreateMap<Users, UsersDTO>();
+
+            CreateMap<InventoryDTO, Inventory>();
+            CreateMap<Inventory, InventoryDTO>();
+
+            CreateMap<ProductSuppliersDTO, ProductSuppliers>();
+            CreateMap<ProductSuppliers, ProductSuppliersDTO>();
+
+            CreateMap<ProductsDTO, Products>();
+            CreateMap<Products, ProductsDTO>();
+
+            CreateMap<StoresDTO, Stores>();
+            CreateMap<Stores, StoresDTO>();
+
+            CreateMap<CategoriesDTO, Categories>();
+            CreateMap<Categories, CategoriesDTO>();
+
+            CreateMap<SuppliersDTO, Suppliers>();
+            CreateMap<Suppliers, SuppliersDTO>();
+
+            CreateMap<LoyaltyProgramsDTO, LoyaltyPrograms>();
+            CreateMap<LoyaltyPrograms, LoyaltyProgramsDTO>();
+
+            CreateMap<SalesDetailsResponseDTO, SalesDetails>();
+            CreateMap<SalesDetails, SalesDetailsResponseDTO>();
+
+            CreateMap<SalesResponseDTO, Sales>();
+            CreateMap<Sales, SalesResponseDTO>();
+
+            CreateMap<UserLoyaltyResponseDTO, UserLoyalty>();
+            CreateMap<UserLoyalty, UserLoyaltyResponseDTO>();
+
+            CreateMap<ProductRecommendationsResponseDTO, ProductRecommendations>();
+            CreateMap<ProductRecommendations, ProductRecommendations>();
+
+            CreateMap<UserStoreResponseDTO, UserStore>();
+            CreateMap<UserStore, UserStoreResponseDTO>();
+
+            CreateMap<CustomerFeedbackResponseDTO, CustomerFeedback>();
+            CreateMap<CustomerFeedback, CustomerFeedbackResponseDTO>();
+
+            CreateMap<UsersResponseDTO, Users>();
+            CreateMap<Users, UsersResponseDTO>();
+
+            CreateMap<InventoryResponseDTO, Inventory>();
+            CreateMap<Inventory, InventoryResponseDTO>();
+
+            CreateMap<ProductSuppliersResponseDTO, ProductSuppliers>();
+            CreateMap<ProductSuppliers,ProductSuppliersResponseDTO>();
+
+            CreateMap<ProductsResponseDTO, Products>();
+            CreateMap<Products, ProductsResponseDTO>();
+
+            CreateMap<StoresResponseDTO, Stores>();
+            CreateMap<Stores, StoresResponseDTO>();
+
+            CreateMap<CategoriesResponseDTO, Categories>();
+            CreateMap<Categories, CategoriesResponseDTO>();
+
+            CreateMap<SuppliersResponseDTO, Suppliers>();
+            CreateMap<Suppliers, SuppliersResponseDTO>();
+
+            CreateMap<LoyaltyProgramsResponseDTO, LoyaltyPrograms>();
+            CreateMap<LoyaltyPrograms, LoyaltyProgramsResponseDTO>();
+
+        }
+    }
+}
