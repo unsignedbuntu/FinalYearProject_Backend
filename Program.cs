7cb901ec835de703a49a8214d3fd76190b3e710f
atalay <atalaybeyazit16@gmail.com>
2024-11-05 16:25:01 +0300

    Program.cs ve appsettings.json dosyası tamamlanıldı.

--- Program.cs
+++ Program.cs
@@ -1,36 +1,73 @@
+using KTUN_Final_Year_Project;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using System.Text.Json.Serialization;
+using AutoMapper;
+using Microsoft.Extensions.Options;
+using System.ComponentModel;
+using System.Globalization;
+using System.Text.Json;
 
-namespace KTUN_Final_Year_Project
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAutoMapper(typeof(Program));
+
+builder.Services.AddCors(options =>
 {
-    public class Program
+    options.AddPolicy("AllowAll",
+        builder => builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
+
+builder.Services.AddControllers().AddJsonOptions(opt =>
+{
+    opt.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    opt.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+
+});
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.CustomSchemaIds(type => type.ToString());
+
+});
+
+
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "KTUN API", Version = "v1" });
+    c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+    c.SupportNonNullableReferenceTypes();
+    c.MapType<object>(() => new OpenApiSchema { Type = "object" });
+});
+
+var connectionString = builder.Configuration.GetConnectionString("KTUN_DbContext");
+
+builder.Services.AddDbContext<KTUN_DbContext>(options =>
+  options.UseSqlServer(builder.Configuration.GetConnectionString("KTUN_DbContext") ?? throw new InvalidOperationException("Connection string 'KTUN_DbContext' not found in configuration.")));
+
+var app = builder.Build();
+
+app.UseCors("AllowAll");
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "KTUN API");
 
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
+    });
 }
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
+
